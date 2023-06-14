cb973f582d2f37893419e182eeccd5d236a497e0
Samuel-Mat <89417260+Samuel-Mat@users.noreply.github.com>
2023-06-14 08:17:49 +0200

    Add files via upload

---
+++ ZeitrechnerCode/ZeitrechnerCode/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZeitrechnerCode.Model;
+
+namespace ZeitrechnerCode.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("{datum}")]
+        public async Task<ActionResult<Zeitrechner>> GetZeit(DateTime datum)
+        {
+            Zeitrechner zeitrechner = new Zeitrechner();
+            zeitrechner.Datum = datum;
+            
+
+            return Ok(zeitrechner);
+        }
+        
+        
+    }
+}
